a9ad9b9fd05c572f3693c8f7999aa47dee6e9ce5
Frederico Martins <fredericomartins@outlook.com>
2018-07-19 16:28:05 +0100

    Update retrieve_machine_addresses.py

--- scripts/retrieve_machine_addresses.py
+++ scripts/retrieve_machine_addresses.py
@@ -3,24 +3,13 @@ from socket import error, inet_aton
 from subprocess import PIPE, Popen
 from textwrap import dedent
 
+from ..inventory import hosts
 from ..vSphere_automation import vSphere
 
 
 api = vSphere(getuser(), getpass())
-stdout, status = Popen('''
-for each in `cat hosts | grep -v '^\(\[.*\]$\|#\)' | xargs`; do
-    if [[ $each = *"["* ]] && [[ $each = *"]"* ]]; then
-        for each in $(eval echo $(sed 's/\[/{/g' <<<${each} | sed 's/]/}/g' | sed 's/:/../g' )); do
-            echo $each
-        done
-    else
-        echo $each
-    fi
-done
-''', shell=True, stdout=PIPE, stderr=PIPE).communicate()
 
-
-for vm in api.GetVirtualMachines(filter=lambda vm: vm.name in stdout):
+for vm in api.GetVirtualMachines(filter=lambda vm: vm.name in hosts()):
     for n, nic in enumerate(vm.guest.net):
         addresses = nic.ipConfig.ipAddress
 
